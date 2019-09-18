cd6039a9bcc0b8f49a38720c58bdd6055f355364
Alejandro Roa <a.roa@fixme.es>
2019-09-17 20:44:01 -0500

    init

---
+++ app/Http/Controllers/TodoController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Todo;
+
+class TodoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $todos = Todo::all();
+        return $todos;
+    }
+
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $todo = Todo::create([
+            'name' => $request->name,
+            'date' => $request->date,
+        ]);
+
+        return $todo;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $todo = Todo::find($id);
+        return $todo;
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $todo = Todo::find($id);
+        $todo->name = $request->name;
+        $todo->date = $request->date;
+        $todo->save();
+        
+        return $todo;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Todo::destroy($id);
+        
+        return $id;
+    }
+}
